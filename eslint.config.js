638ea25ca6a739d1d25d9c98519b66fc00492108
CaramelMoka <101611218+CaramelMoka@users.noreply.github.com>
2026-03-23 03:07:22 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'spec', '**/*.spec.cjs]),
+  globalIgnores(['dist', 'spec', '**/*.spec.cjs']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
